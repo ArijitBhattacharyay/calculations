5201964598b8e662c32fca4e77c4cc06407fa766
Srinjan Ghosh <srinjanghosh11@gmail.com>
2019-09-03 00:24:40 +0530

    completed squares part

--- js/index.js
+++ js/index.js
@@ -1,47 +1,95 @@
+let counter = 10;
+let timer;
+let totalQuestions = 2;
+let question = 0;
+let questions = new Array(0);
+let answers = new Array(0);
+
 $('.squares').click(function(){
     $('.display').css('display','block');
     $('.description').append("<p>One number will be given. You have to square the Number</p>");
-})
-
-let counter = 10;
-let timer;
-
-function countDown(){
-    counter--;
-    $('.timer').html(`Time : ${counter}`);
-    if(counter === 0){
-        clearInterval(timer);
-        // TODO: display result
-    }
-}
+});
 
 $('.btn-go').click(function(){
     $('.display').css('display','none');
     $('.playground').css('display','block');
+
+    for(let i = 0;i<totalQuestions;i++){
+        let n = Math.floor(Math.random()*10 + 1);
+        questions.push(n);
+        let html = `<div class="row number-${i} mt-5">
+                        <div class="col-6 text-center">
+                            <p>Number:${n}</p>
+                        </div>
+                        <div class="col-6 text-center">
+                            <span>Answer:</span>
+                            <input type="number" class="answer-${i}">
+                        </div>
+                    </div>`;
+        $('.playground').append(html);
+    }
+    for(let i=1;i<totalQuestions;i++){
+        $(`.number-${i}`).css('display','none');
+    }
     $('.timer').html(`Time : ${counter}`);
 
     timer = setInterval(countDown,1000);
-    generateQuestion();
+    getInput();
 });
 
-function check(ans){
-    $('input[type=number]').keydown(function(event){
-        if(event.which == 13){
-            if($('input[type=number]').val()!="")
-            userAns = $('input[type=number]').val();
-            if(userAns == ans){
-                $('input[type=number]').val("");
-                generateQuestion();
-            }
-            $('input[type=number]').focus();
+function countDown(){
+    if(question == totalQuestions){
+        clearInterval(timer);
+        displayResults();
+    }
+    counter--;
+    $('.timer').html(`Time : ${counter}`);
+    if(counter === 0){
+        clearInterval(timer);
+        $('input[type=number]').prop('disabled', true);
+        if($(`.answer-${question}`).val()!="" || $(`.answer-${question}`).val()!= undefined){
+            answers.push($(`.answer-${question}`).val());
         }
-    })
+        displayResults();
+    }
 }
 
-function generateQuestion(){
-    let n = Math.floor(Math.random()*10 +1);
-    $('.number > p').text(`Number: ${n}`);
+function getInput(){
     $('input[type=number]').focus();
-    let ans = n*n;
-    check(ans);
+    $('input[type=number]').keydown(function(event){
+        if(event.which == 13){
+            event.preventDefault();
+            if($(`.answer-${question}`).val()!=""){
+                answers.push($(`.answer-${question}`).val());
+            }
+            loadNextQuestion();
+        }
+    });
+}
+
+function loadNextQuestion() {
+    $(`.number-${question}`).css('display','none');
+    question++;
+    $(`.number-${question}`).css('display','flex');
+    $('input[type=number]').focus();
+
+}
+
+function displayResults(){
+    $('.playground').css('display','none');
+    $('.result').css('display','block');
+    for(let i=0;i<answers.length;i++){
+        if(answers[i] == questions[i]*questions[i])    {right = 'success'; icon = '<i class="fa fa-check float-right"></i>'}
+        else    {right = 'danger'; icon='<i class="fa fa-times float-right"></i>';}
+        let html = `<div class="card text-center text-white bg-${right} mb-3">
+                <div class="card-body">
+                    <span class="card-text">
+                        <span class="float-left">${i+1}.   Number ${questions[i]}</span>
+                        <span>Your Answer: ${answers[i]}</span>
+                        ${icon}
+                    </span> 
+                </div>
+            </div>`;
+        $('.result').append(html);
+    }
 }
